f466c557ac333c1fa4c4e572ce9f3e3e0ca2fbfa
artmikula <artm87@gmail.com>
2021-09-10 15:25:50 +0900

    Last controller file was wrong

--- controller/controller.php
+++ controller/controller.php
@@ -5,16 +5,16 @@ require("./model/search.php");
 function showAllEvents() {
     $events = showAllEvents();
     require("./view/eventView.php");
-}
+};
 
 function searchAllEvents() {
     $events = searchAllEvents();
     require("./view/eventView.php");
-    require("./view/searchForm.php")
-}
+    require("./view/searchForm.php");
+};
 
 function filterEvents() {
     $events = filterEvents();
     require("./view/eventView.php");
-    require("./view/searchForm.php")
-}
+    require("./view/searchForm.php");
+};
